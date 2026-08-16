287c7b1c8442a81207c518cd47ed4eeec0b52175
Xiang Wu <18064248+xiangwuxw@users.noreply.github.com>
2026-08-16 10:24:07 -0700

    Refactor winiso2usb.ps1 for improvements and fixes

--- winiso2usb.ps1
+++ winiso2usb.ps1
@@ -6,6 +6,45 @@
 #          and full PowerShell Write-Progress tracking (Completed/Total files).
 #>
 
+<#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#>
+
+#
+# How to run 
+# 1. Have one iso in the folder of the script
+# 2. Plug in an USB < 32G
+# 3. Powershell -ExecutionPolicy Bypass -File winiso2usb.ps1
+# 
+
+<#   ###########################################################
+#    ISSSUE RESOLVED IN THIS SCRIPT: WIM FILE > 4G and UEFI Boot. 
+# 
+# 1. Though some UEFI implementation may support NTFS, most of the them only support FAT/FAT32, 
+# 2. FAT32 max file size is 4G,
+# 3. Windows WIM file is too big, even the install.wim on retail DVD may > 4G
+# 4. Most captured WIM file are super large, sometime > 10G
+# 
+#    ######## 
+#    SOLUTION
+#
+# 1. Create two partitions in the USB disk, first FAT32 for UEFI Boot, boot.wim to load the WINPE or Setup Environment
+# 2. Windows Setup will look for the install.wim on all volumes, so put the install.wim on the second NTFS partition
+# 3. Though UEFI doesn't check active partition, that set NTFS partition active will allow legacy BIOS to boot directly.  
+#    In this script, set the FAT32 active, so no matter UEFI or Legacy will boot from the same code/file FAT32 path to make it easier to maintain the code.
+#>
+
+#
+# Modify the policy to prevent annoying shell/explorer pop-ups. Once done, remove the setting from registry
+#
+# New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name NoDriveTypeAutoRun  -value 255 -type Dword
+# Remove-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name NoDriveTypeAutoRun
+
 #Requires -RunAsAdministrator
 
 #
